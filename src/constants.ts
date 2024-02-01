b0652c968bbee0ecd2c41f8b8e63a351ae201f05
unknown <drmeloy@gmail.com>
2024-01-31 23:35:37 -0800

    fixed mongodb name in uri

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const secretKey = process.env.JWT_SECRET || 'defaultSecretKey';
 
 const mongoPW = process.env.MONGO_PASSWORD;
-export const mongoURI = `mongodb+srv://drmeloy:${mongoPW}@leaguecal.f9nbvhw.mongodb.net/?retryWrites=true&w=majority`
+export const mongoURI = `mongodb+srv://drmeloy:${mongoPW}@leaguecal.f9nbvhw.mongodb.net/leaguecal?retryWrites=true&w=majority`
